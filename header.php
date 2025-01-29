d65167db9f718ac97e44a314588738b9ec7bd3c3
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-01-28 21:13:45 -0500

    Update header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>IDM 250</title>
+</head>
+<body>
+	<header>
+		<ul>
+		<li><a href="index.php">Vy Le</a></li>
+	</ul>
+		<nav>
+		<ul>
+			<li><a href="works.php">Work</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="resume.php">Resume</a></li>
+		</ul>
+		</nav>
+	</header>
+	<h1>idm250</h1>
+</body>
+</html>
