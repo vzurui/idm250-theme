46e0e9f234d3ae04f7d52209c7c1bed9be947a67
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-01-28 22:37:43 -0500

    added footer

--- footer.php
+++ footer.php
@@ -4,10 +4,25 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>IDM250</title>
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="./wp-content/themes/idm250-theme/style.css">
 <title>IDM 250</title>
 </head>
 <body>
-	<h2>hello</h2>
+<footer>
+  <div class=footer-contents>
+  <img src="placeholder-circle.png">
+   <div class="social-icons">
+      <img src="linkedin.png">
+      <img src="email.png">
+   </div>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+     <li><a href="works.php">Work</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="resume.php">Resume</a></li>
+  </ul>
+  </div>
+  <p>©2025</p>
+</footer>
 </body>
 </html>
