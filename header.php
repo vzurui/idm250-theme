8e23a5b2abbeb9b9277fe5db75b633a600648b6f
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-01-28 23:20:04 -0500

    updates

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
   <input class="menu-btn" type="checkbox" id="menu-btn" />
   <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
   <ul class="menu">
-    <li><a href="./wp-content/themes/idm250-theme/footer.php">Work</a></li>
+    <li><a href="./wp-content/themes/idm250-theme/works.php">Work</a></li>
     <li><a href="./wp-content/themes/idm250-theme/about.php">About</a></li>
     <li><a href="./wp-content/themes/idm250-theme/resume.php">Resume</a></li>
   </ul>
--- index.php
+++ index.php
@@ -7,11 +7,9 @@
 <link rel="stylesheet" href="./wp-content/themes/idm250-theme/style.css">
 </head>
 <body>
-<?php include 'header.php';?>
 
-<h1>idm250</h1>
+
 
 <? include 'footer.php';?>
-
 </body>
 </html>
