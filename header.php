7fb4d429e0a41bcbb74904109925d81057943a83
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-01-28 22:05:44 -0500

    updates

--- header.php
+++ header.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="style.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>IDM250</title>
+<link rel="stylesheet" href="./wp-content/themes/idm250-theme/style.css">
 <title>IDM 250</title>
 </head>
 <body>
