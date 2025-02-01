be358c89163f04da98c9197fec1531637230cd2e
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-02-01 14:07:35 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>IDM250</title>
+<link rel="stylesheet" href="./wp-content/themes/idm250-theme/style.css">
+<title>IDM 250</title>
+</head>
+<body>
 <header class="header">
   <a href="./wp-content/themes/idm250-theme/index.php" class="logo">Vy Le</a>
   <input class="menu-btn" type="checkbox" id="menu-btn" />
@@ -8,3 +18,5 @@
     <li><a href="./wp-content/themes/idm250-theme/resume.php">Resume</a></li>
   </ul>
 </header>
+</body>
+</html>
