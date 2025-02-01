152ee7e05f567b999e359cdb4d419fc574f38bda
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-02-01 14:05:18 -0500

    updates

--- footer.php
+++ footer.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>IDM250</title>
-<link rel="stylesheet" href="./wp-content/themes/idm250-theme/style.css">
-<title>IDM 250</title>
-</head>
-<body>
 <footer>
   <div class=footer-contents>
   <img src="./wp-content/themes/idm250-theme/images/placeholder-circle.png" class="logo">
--- index.php
+++ index.php
@@ -1,17 +1,2 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>IDM250</title>
-<link rel="stylesheet" href="./wp-content/themes/idm250-theme/style.css">
-</head>
-<body>
-<header>
-<?php include 'header.php';?>
-</header>
-<footer>
-<?php include 'footer.php';?>
-</footer>
-</body>
-</html>
+<?php get_header(); ?>
+<?php get_footer(); ?>
