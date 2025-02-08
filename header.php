4155e9514dc49857b0297e6ef07fb1f258dccba3
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-02-07 23:38:38 -0500

    updates

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>IDM250</title>
-<link rel="stylesheet" href="./wp-content/themes/idm250-theme/style.css">
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 <title>IDM 250</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php get_header(); ?>
+
+<main class="post-list">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article class="post">
+            <h2 class="post-title">
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h2>
+            <p class="post-meta">Published on <?php the_date(); ?> in <?php the_category(', '); ?></p>
+            <?php if (has_post_thumbnail()) : ?>
+                <a href="<?php the_permalink(); ?>">
+                    <div class="post-thumbnail">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                </a>
+            <?php endif; ?>
+            <div class="post-excerpt">
+                <?php the_excerpt(); ?>
+            </div>
+        </article>
+    <?php endwhile; else: ?>
+        <p>No posts found.</p>
+    <?php endif; ?>
+</main>
+
 <?php get_footer(); ?>
