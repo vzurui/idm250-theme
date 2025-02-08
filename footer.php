6b5ca54663d607cb6cbbcfde9f517028cd340dac
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-02-07 23:48:22 -0500

    updates

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer>
   <div class=footer-contents>
-  <img src="./wp-content/themes/idm250-theme/images/placeholder-circle.png" class="logo">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder-circle.png" alt="Placeholder Circle Image" class="logo">
    <div class="social-icons">
-      <img src="./wp-content/themes/idm250-theme/images/linkedin.png">
-      <img src="./wp-content/themes/idm250-theme/images/email.png">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/linkedin.png" alt="LinkedIn">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/email.png" alt="Email">
    </div>
   <ul>
     <li><a href="./wp-content/themes/idm250-theme/index.php">Home</a></li>
