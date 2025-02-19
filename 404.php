782ce053338fef91f00a133c27db327c0a5aa6cf
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-02-18 22:44:38 -0500

    updates

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<h1>Oops!</h1>
+<h1>404</h1>
+<h1>Page Not Found</h1>
+
+<a href="<?php echo home_url(); ?>" class="button">Back to Home</a>
+<?php get_footer(); ?>
--- footer.php
+++ footer.php
@@ -1,16 +1,25 @@
 <footer>
-  <div class=footer-contents>
-  <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder-circle.png" alt="Placeholder Circle Image" class="logo">
-   <div class="social-icons">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/linkedin.png" alt="LinkedIn">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/email.png" alt="Email">
-   </div>
-  <ul>
-    <li><a href="./wp-content/themes/idm250-theme/index.php">Home</a></li>
-    <li><a href="./wp-content/themes/idm250-theme/footer.php">Work</a></li>
-    <li><a href="./wp-content/themes/idm250-theme/about.php">About</a></li>
-    <li><a href="./wp-content/themes/idm250-theme/resume.php">Resume</a></li>
-  </ul>
+<div class="footer-contents">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder-circle.png" alt="Placeholder Circle Image" class="logo">
+    
+    <div class="social-icons">
+      <a href="https://www.linkedin.com/in/yourprofile" target="_blank">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/linkedin.png" alt="LinkedIn">
+      </a>
+      <a href="mailto:your@email.com">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/email.png" alt="Email">
+      </a>
+    </div>
+   <ul>
+      <li><a href="<?php echo home_url(); ?>">Home</a></li>
+      <li><a href="<?php echo site_url('/work'); ?>">Work</a></li>
+      <li><a href="<?php echo site_url('/about'); ?>">About</a></li>
+      <li><a href="<?php echo site_url('/resume'); ?>">Resume</a></li>
+    </ul>
   </div>
-  <p>©2025</p>
+
+<p>© <?php echo date('Y'); ?> Vy Le. All Rights Reserved.</p>
+<?php wp_footer(); ?>
+</body>
 </footer>
+</html>
