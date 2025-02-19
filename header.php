fbf49135d507ec577c93bb25f60912b89800351e
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-02-18 22:10:53 -0500

    Update header.php
    added in body class and wp_head

--- header.php
+++ header.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>IDM250</title>
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 <title>IDM 250</title>
+<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <header class="header">
-  <a href="./wp-content/themes/idm250-theme/index.php" class="logo">Vy Le</a>
+<a href="<?php echo home_url(); ?>" class="logo">Vy Le</a>
   <input class="menu-btn" type="checkbox" id="menu-btn" />
   <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
   <ul class="menu">
-    <li><a href="./wp-content/themes/idm250-theme/works.php">Work</a></li>
-    <li><a href="./wp-content/themes/idm250-theme/about.php">About</a></li>
-    <li><a href="./wp-content/themes/idm250-theme/resume.php">Resume</a></li>
-  </ul>
-</header>
+        <li><a href="<?php echo site_url('/works'); ?>">Work</a></li>
+        <li><a href="<?php echo site_url('/about'); ?>">About</a></li>
+        <li><a href="<?php echo site_url('/resume'); ?>">Resume</a></li>
+    </ul>
+  </header>
 </body>
 </html>
