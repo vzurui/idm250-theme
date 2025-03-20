d56d03a8626156d8e4fc669ab3607e599802e5ac
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-03-20 04:13:05 -0400

    updates
    updated header

--- page-sidebar.php
+++ page-sidebar.php
@@ -8,21 +8,27 @@ get_header();
 
 <div class="sidebar-page-container">
     <main class="main-content">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-            <!-- Featured Image -->
-            <?php if (has_post_thumbnail()) : ?>
-                <div class="featured-image">
-                    <?php the_post_thumbnail('large'); ?>
-                </div>
-            <?php endif; ?>
-
-            <h1><?php the_title(); ?></h1>
-            <div class="content">
-                <?php the_content(); ?>
+        <?php if (has_post_thumbnail()) : ?>
+            <div class="featured-image">
+                <?php the_post_thumbnail('large'); ?>
             </div>
+        <?php endif; ?>
 
-        <?php endwhile; endif; ?>
+        <?php if (!is_page('resume')) : ?>
+            <h1><?php the_title(); ?></h1>
+        <?php endif; ?>
+
+        <div class="content">
+            <?php 
+            if (have_posts()) : 
+                while (have_posts()) : the_post(); 
+                    the_content(); 
+                endwhile; 
+            else : 
+                echo '<p>No content found.</p>';
+            endif; 
+            ?>
+        </div>
     </main>
 
     <aside class="sidebar">
