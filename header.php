55d568b096b95d98f51428389e4e4b5cb7c201d8
Vy Le <139026534+vzurui@users.noreply.github.com>
2025-03-19 23:30:24 -0400

    updates
    favicon

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>IDM 250</title>
+
+<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png" type="image/x-icon">
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
